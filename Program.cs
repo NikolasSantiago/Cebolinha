f5ff4ecb010f4dd5e9b17d48a6a6d46854a709c8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-19 15:41:52 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string fraseDigitada; 
+string fraseLeet;
+
+ Console.Clear();
+
+  Console.Write(" Digite uma frase para o Cebola :");
+
+   fraseDigitada = Console.ReadLine()!;
+
+    fraseLeet = fraseDigitada 
+    .Replace("r", "l") .Replace("R", "L");
+
+     Console.WriteLine(fraseLeet);
